f40c84733a968d4c0e73ceedc8e69613b291967d
hwanseok.dev <hwanseok.dev@gmail.com>
2021-03-22 14:13:55 +0900

    apply ErrorDto

--- src/main/java/me/hwanseok/simplemsaproduct/advice/ExceptionHandlerAdvice.java
+++ src/main/java/me/hwanseok/simplemsaproduct/advice/ExceptionHandlerAdvice.java
@@ -2,6 +2,7 @@ package me.hwanseok.simplemsaproduct.advice;
 
 import me.hwanseok.simplemsaproduct.exception.ProductConstraintViolationException;
 import me.hwanseok.simplemsaproduct.exception.ProductNotFoundException;
+import me.hwanseok.simplemsaproduct.model.dto.common.ErrorDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,12 +10,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<String> notFoundException(ProductNotFoundException e){
-        return ResponseEntity.badRequest().body("Product Not Found");
+    public ResponseEntity<ErrorDto> notFoundException(ProductNotFoundException e){
+        return ResponseEntity.badRequest().body(ErrorDto.builder()
+                .msg("Product Not Found")
+                .build());
     }
 
     @ExceptionHandler(ProductConstraintViolationException.class)
-    public ResponseEntity<String> constraintViolationException(ProductConstraintViolationException e){
-        return ResponseEntity.badRequest().body("Constraint Violation");
+    public ResponseEntity<ErrorDto> constraintViolationException(ProductConstraintViolationException e){
+        return ResponseEntity.badRequest().body(ErrorDto.builder()
+                .msg("Constraint Violation")
+                .build());
     }
 }
